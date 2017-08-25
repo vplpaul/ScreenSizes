95d05c1bffb79f67d847858649d40325a799ac9e
vplpaul <vplpaul@gmail.com>
2017-08-25 18:53:41 +0530

    First Video

---
+++ Dev_Profiles/Dev_Profiles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dev_Profiles
+//
+//  Created by Vipul Paul on 25/08/17.
+//  Copyright © 2017 Jetpack Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
